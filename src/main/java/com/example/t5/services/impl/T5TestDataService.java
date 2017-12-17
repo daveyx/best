3dd41045fe2faf32c6d1ee9ead1b6f4148b5419f
daveyx <d.rehle@gmail.com>
2017-12-17 02:21:48 +0100

    fixed gettestdata

--- src/main/java/com/example/t5/services/impl/T5TestDataService.java
+++ src/main/java/com/example/t5/services/impl/T5TestDataService.java
@@ -1,6 +1,8 @@
 package com.example.t5.services.impl;
 
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.example.elasticsearch.model.TestData;
 import com.example.elasticsearch.repo.TestDataRepository;
@@ -13,7 +15,10 @@ public class T5TestDataService implements IT5TestDataService {
 
 	@Override
 	public TestData getTestDataByUuid(final String uuid) {
-//		return testDataRepository.findByUuid(uuid);
+		final Page<TestData> testDataPage = testDataRepository.findByUuId(TestData.TEST_UUID, Pageable.unpaged());
+		if (testDataPage.getContent().isEmpty() == false) {
+			return testDataPage.getContent().get(0);
+		}
 		return null;
 	}
 
