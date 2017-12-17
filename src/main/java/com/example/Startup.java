5c95c6d7c7027fa870376015895d23ce413563e7
daveyx <d.rehle@gmail.com>
2017-12-17 02:09:26 +0100

    init testdata

--- src/main/java/com/example/Startup.java
+++ src/main/java/com/example/Startup.java
@@ -5,8 +5,11 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import com.example.elasticsearch.model.TestData;
 import com.example.elasticsearch.repo.TestDataRepository;
 
 @Component
@@ -14,12 +17,12 @@ public class Startup {
 
 	private static final Log LOGGER = LogFactory.getLog(Startup.class);
 
-//	@Autowired
-//	private TestDataRepository testDataRepository;
+	@Autowired
+	private TestDataRepository testDataRepository;
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void doSomethingAfterStartup() {
-//		initTestData();
+		initTestData();
 		LOGGER.info("-------------> startup finished");
 	}
 
@@ -28,17 +31,22 @@ public class Startup {
 	//
 
 
-//	private void initTestData() {
-//		final UUID uuid = UUID.fromString(TestData.TEST_UUID);
-//		final TestData testDataExisting = testDataRepository.findByUuid(TestData.TEST_UUID);
-//		if (testDataExisting != null) {
-//			testDataRepository.deleteAll();
-//		}
-//		final TestData testData = new TestData();
-//		testData.setId(uuid);
-//		testData.setUuid(TestData.TEST_UUID);
-//		testData.setValue("1");
-//		final TestData testDataSaved = testDataRepository.save(testData);
-//		LOGGER.info("testdata.uuid=" + testDataSaved.getId());
-//	}
+	private void initTestData() {
+		final String uuid = TestData.TEST_UUID;
+		final Page<TestData> testDataPage = testDataRepository.findByUuId(TestData.TEST_UUID, Pageable.unpaged());
+		final TestData testDataExisting;
+		if (testDataPage.getContent().isEmpty() == false) {
+			testDataExisting = testDataPage.getContent().get(0);
+		} else {
+			testDataExisting = null;
+		}
+		if (testDataExisting != null) {
+			testDataRepository.deleteAll();
+		}
+		final TestData testData = new TestData();
+		testData.setUuId(uuid);
+		testData.setValue("1");
+		final TestData testDataSaved = testDataRepository.save(testData);
+		LOGGER.info("testdata.uuid=" + testDataSaved.getId());
+	}
 }
