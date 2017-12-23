41791e44b62461650228ce21414638ce3d392dc4
daveyx <d.rehle@gmail.com>
2017-12-23 16:25:33 +0100

    externalized messages

--- src/main/java/com/example/t5/pages/Register.java
+++ src/main/java/com/example/t5/pages/Register.java
@@ -14,6 +14,7 @@ import org.apache.tapestry5.annotations.SetupRender;
 import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.corelib.components.PasswordField;
 import org.apache.tapestry5.corelib.components.TextField;
+import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.tynamo.security.services.SecurityService;
 
@@ -33,16 +34,19 @@ public class Register {
 	@Inject
 	private AlertManager alertManager;
 
+	@Inject
+	private Messages messages;
+
 	// -----------> components
 
 	@InjectComponent
 	private Form registrationForm;
 
-    @Component(id = "registerEmail", parameters = "validate=required,email")
-    private TextField registerEmailField;
+	@Component(id = "registerEmail", parameters = "validate=required,email")
+	private TextField registerEmailField;
 
-    @Component(id = "registerPassword", parameters = "validate=required")
-    private PasswordField registerPasswordField;
+	@Component(id = "registerPassword", parameters = "validate=required")
+	private PasswordField registerPasswordField;
 
 	// -----------> properties
 
@@ -65,14 +69,14 @@ public class Register {
 	void onValidateFromRegistrationForm() {
 
 		if (StringUtils.isNotBlank(registerPassword) && !registerPassword.matches("^[A-Za-z0-9]+$")) {
-			registrationForm.recordError(registerPasswordField, "Fehler - bitte keine Sonderzeichen benutzen.");
-			alertManager.error(
-					"Für das Passwort sind nur die Zeichen A-Z a-z und 0-9 erlaubt! Bitte keine Sonderzeichen benutzen.");
+			registrationForm.recordError(registerPasswordField,
+					messages.get("com.example.registration.passwordformat1"));
+			alertManager.error(messages.get("com.example.registration.passwordformat2"));
 		}
 
 		if (accountService.isEmailExisting(registerEmail)) {
-			registrationForm.recordError(registerEmailField, "Fehler - bitte eine andere E-Mail Adresse benutzen.");
-			alertManager.error("Diese E-Mail Adresse ist nicht erlaubt. Btte eine andere E-Mail Adresse benutzen.");
+			registrationForm.recordError(registerEmailField, messages.get("com.example.registration.emailnotalowed1"));
+			alertManager.error(messages.get("com.example.registration.emailnotalowed2"));
 		}
 	}
 
@@ -81,8 +85,8 @@ public class Register {
 			accountService.register(registerEmail, registerPassword, registerNewsLetter);
 		} catch (final TrashMailException e) {
 			e.printStackTrace();
-			registrationForm.recordError(registerEmailField, "Fehler - bitte eine andere E-Mail Adresse benutzen.");
-			alertManager.error("Diese E-Mail Adresse ist nicht erlaubt. Btte eine andere E-Mail Adresse benutzen.");
+			registrationForm.recordError(registerEmailField, messages.get("com.example.registration.emailnotalowed1"));
+			alertManager.error(messages.get("com.example.registration.emailnotalowed2"));
 			return null;
 		}
 		final String loginMessage = doLogin(registerEmail, registerPassword);
@@ -90,19 +94,16 @@ public class Register {
 			alertManager.error(loginMessage);
 			return null;
 		} else {
-			alertManager.success("Bitte bestätigen Sie die Registrierung durch Klick auf den Link in der E-Mail welche wir Ihnen zugesendet haben.");
+			alertManager.success("com.example.registration.success");
 			return Index.class;
 		}
 	}
-	
-	
+
 	//
 	// ---> private
 	//
-	
-	
-	private String doLogin(final String email, final String password)
-	{
+
+	private String doLogin(final String email, final String password) {
 		String loginErrorMessage = null;
 
 		final Subject currentUser = securityService.getSubject();
@@ -113,21 +114,21 @@ public class Register {
 		final UsernamePasswordToken token = new UsernamePasswordToken(email, password);
 		try {
 			currentUser.login(token);
-//		}
-//		catch (final UnknownAccountException e) {
-//			if (_registrationService.isAccountNotAcknowledged(email)){
-//				throw new AccountNotAcknowledgedException();
-//			} else {
-//				loginErrorMessage = "Dieses Konto existiert nicht.";
-//			}
-		} catch (IncorrectCredentialsException e) {
-			loginErrorMessage = "Falsches Passwort.";
-		} catch (LockedAccountException e) {
-			loginErrorMessage = "Dieses Konto ist inaktiv. Bitte kontaktieren Sie uns.";
-		} catch (AuthenticationException e) {
-			loginErrorMessage = "AuthenticationError";
+			// }
+			// catch (final UnknownAccountException e) {
+			// if (_registrationService.isAccountNotAcknowledged(email)){
+			// throw new AccountNotAcknowledgedException();
+			// } else {
+			// loginErrorMessage = "Dieses Konto existiert nicht.";
+			// }
+		} catch (final IncorrectCredentialsException e) {
+			loginErrorMessage = messages.get("com.example.registration.IncorrectCredentialsException");
+		} catch (final LockedAccountException e) {
+			loginErrorMessage = messages.get("com.example.registration.LockedAccountException");
+		} catch (final AuthenticationException e) {
+			loginErrorMessage = messages.get("com.example.registration.AuthenticationException");
 		}
-		
+
 		return loginErrorMessage;
 	}
 
