37b94e00267e877dedf7ee431bf974443bec08ea
daveyx <d.rehle@gmail.com>
2017-12-25 01:53:38 +0100

    a bit admin layout

--- src/main/java/com/example/t5/admin/pages/AdminArticleGroup.java
+++ src/main/java/com/example/t5/admin/pages/AdminArticleGroup.java
@@ -2,29 +2,25 @@ package com.example.t5.admin.pages;
 
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.tapestry5.annotations.Component;
-import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
 import com.example.t5.admin.components.AdminLayout;
 
 @RequiresAuthentication
-public class AdminIndex {
-
+public class AdminArticleGroup {
+	
 	// -----------> services
 
-	@Inject
-	private JavaScriptSupport javaScriptSupport;
-
+	
+	
 	// -----------> components
-
+	
 	@Component(id = "template", parameters = {})
 	private AdminLayout layoutComponent;
-
+	
+	
 	// -----------> properties
+	
 
 	// -----------> events
 
-	void afterRender() {
-		javaScriptSupport.require("bootstrap/modal");
-	}
 }
--- src/main/java/com/example/t5/admin/pages/AdminArticleGroups.java
+++ src/main/java/com/example/t5/admin/pages/AdminArticleGroups.java
@@ -2,25 +2,30 @@ package com.example.t5.admin.pages;
 
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 
 import com.example.t5.admin.components.AdminLayout;
 
 @RequiresAuthentication
 public class AdminArticleGroups {
-	
+
 	// -----------> services
 
-	
-	
+	@Inject
+	private JavaScriptSupport javaScriptSupport;
+
 	// -----------> components
-	
+
 	@Component(id = "template", parameters = {})
 	private AdminLayout layoutComponent;
-	
-	
+
 	// -----------> properties
-	
 
 	// -----------> events
+
+	void afterRender() {
+		javaScriptSupport.require("bootstrap/modal");
+	}
 
 }
