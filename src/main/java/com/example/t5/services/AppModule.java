e8776c702430ff2a86dfccdf1c4429aeb35f12bb
daveyx <d.rehle@gmail.com>
2017-12-28 10:00:03 +0100

    code format

--- src/main/java/com/example/t5/services/AppModule.java
+++ src/main/java/com/example/t5/services/AppModule.java
@@ -27,8 +27,8 @@ public class AppModule {
 
 	public static void bind(ServiceBinder binder) {
 		binder.bind(IT5Service.class, T5Service.class);
-    	binder.bind(IT5TestDataService.class, T5TestDataService.class);
-    	binder.bind(ShiroRealm.class);
+		binder.bind(IT5TestDataService.class, T5TestDataService.class);
+		binder.bind(ShiroRealm.class);
 	}
 
 	public static void contributeIgnoredPathsFilter(Configuration<String> configuration) {
@@ -36,42 +36,42 @@ public class AppModule {
 		configuration.add("/playground");
 	}
 
-    public static void contributeFactoryDefaults(
-        MappedConfiguration<String, Object> configuration)
-    {
-        // The values defined here (as factory default overrides) are themselves
-        // overridden with application defaults by DevelopmentModule and QaModule.
+	public static void contributeFactoryDefaults(MappedConfiguration<String, Object> configuration) {
+		// The values defined here (as factory default overrides) are themselves
+		// overridden with application defaults by DevelopmentModule and QaModule.
 
-        // The application version is primarily useful as it appears in
-        // any exception reports (HTML or textual).
-        configuration.override(SymbolConstants.APPLICATION_VERSION, "0.0.1-SNAPSHOT");
+		// The application version is primarily useful as it appears in
+		// any exception reports (HTML or textual).
+		configuration.override(SymbolConstants.APPLICATION_VERSION, "0.0.1-SNAPSHOT");
 
-        // This is something that should be removed when going to production, but is useful
-        // in the early stages of development.
-        configuration.override(SymbolConstants.PRODUCTION_MODE, false);
-    }
+		// This is something that should be removed when going to production, but is
+		// useful
+		// in the early stages of development.
+		configuration.override(SymbolConstants.PRODUCTION_MODE, false);
+	}
 
-    public static void contributeApplicationDefaults(
-        MappedConfiguration<String, Object> configuration)
-    {
-        // Contributions to ApplicationDefaults will override any contributions to
-        // FactoryDefaults (with the same key). Here we're restricting the supported
-        // locales to just "en" (English). As you add localised message catalogs and other assets,
-        // you can extend this list of locales (it's a comma separated series of locale names;
-        // the first locale name is the default when there's no reasonable match).
-        configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en");
+	public static void contributeApplicationDefaults(MappedConfiguration<String, Object> configuration) {
+		// Contributions to ApplicationDefaults will override any contributions to
+		// FactoryDefaults (with the same key). Here we're restricting the supported
+		// locales to just "en" (English). As you add localised message catalogs and
+		// other assets,
+		// you can extend this list of locales (it's a comma separated series of locale
+		// names;
+		// the first locale name is the default when there's no reasonable match).
+		configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en");
 
-              // You should change the passphrase immediately; the HMAC passphrase is used to secure
-        // the hidden field data stored in forms to encrypt and digitally sign client-side data.
-        configuration.add(SymbolConstants.HMAC_PASSPHRASE, "com.example.t5.services.TapestryMainModule");
-    }
+		// You should change the passphrase immediately; the HMAC passphrase is used to
+		// secure
+		// the hidden field data stored in forms to encrypt and digitally sign
+		// client-side data.
+		configuration.add(SymbolConstants.HMAC_PASSPHRASE, "com.example.t5.services.TapestryMainModule");
+	}
 
 	@Contribute(SymbolProvider.class)
 	@ApplicationDefaults
-	public static void setupEnvironment(MappedConfiguration<String, Object> configuration)
-	{
-        // Support for jQuery is new in Tapestry 5.4 and will become the only supported
-        // option in 5.5.
+	public static void setupEnvironment(MappedConfiguration<String, Object> configuration) {
+		// Support for jQuery is new in Tapestry 5.4 and will become the only supported
+		// option in 5.5.
 		configuration.add(SymbolConstants.JAVASCRIPT_INFRASTRUCTURE_PROVIDER, "jquery");
 		configuration.add(SymbolConstants.BOOTSTRAP_ROOT, "context:mybootstrap");
 	}
@@ -80,12 +80,11 @@ public class AppModule {
 		configuration.add(sr);
 	}
 
-	public static void contributeSecurityConfiguration(
-			OrderedConfiguration<SecurityFilterChain> configuration,
+	public static void contributeSecurityConfiguration(OrderedConfiguration<SecurityFilterChain> configuration,
 			SecurityFilterChainFactory factory) {
 		// OrderedConfiguration must be named, so they can be overridden later
 		configuration.add("signup-anon", factory.createChain("/authc/signup").add(factory.anon()).build());
-		
+
 		configuration.add("authc_signup", factory.createChain("/authc/signup").add(factory.anon()).build());
 		configuration.add("authc", factory.createChain("/authc/**").add(factory.authc()).build());
 		configuration.add("partlyauthc", factory.createChain("/partlyauthc/**").add(factory.anon()).build());
@@ -93,9 +92,12 @@ public class AppModule {
 		configuration.add("user_signup", factory.createChain("/user/signup").add(factory.anon()).build());
 		configuration.add("user", factory.createChain("/user/**").add(factory.user()).build());
 		configuration.add("roles_user", factory.createChain("/roles/user/**").add(factory.roles(), "user").build());
-		configuration.add("roles_manager", factory.createChain("/roles/manager/**").add(factory.roles(), "manager").build());
-		configuration.add("perms_view", factory.createChain("/perms/view/**").add(factory.perms(), "news:view").build());
-		configuration.add("perms_edit", factory.createChain("/perms/edit/**").add(factory.perms(), "news:edit").build());
+		configuration.add("roles_manager",
+				factory.createChain("/roles/manager/**").add(factory.roles(), "manager").build());
+		configuration.add("perms_view",
+				factory.createChain("/perms/view/**").add(factory.perms(), "news:view").build());
+		configuration.add("perms_edit",
+				factory.createChain("/perms/edit/**").add(factory.perms(), "news:edit").build());
 
 		configuration.add("ports_ssl", factory.createChain("/ports/ssl").add(factory.ssl()).build());
 		configuration.add("ports_9000", factory.createChain("/ports/port9090").add(factory.port(), "9090").build());
@@ -109,10 +111,7 @@ public class AppModule {
 		configuration.add(SymbolConstants.APPLICATION_CATALOG + ".BASE", resource);
 	}
 
-
-	
-	public static void contributeComponentClassResolver(final Configuration<LibraryMapping> configuration)
-	{
+	public static void contributeComponentClassResolver(final Configuration<LibraryMapping> configuration) {
 		configuration.add(new LibraryMapping("admin", "com.example.t5.admin"));
 	}
 }
