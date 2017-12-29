95e467834c1bfaa90e36868b71a8ff36601bcb8b
daveyx <d.rehle@gmail.com>
2017-12-29 01:59:44 +0100

    logout; optimized header

--- src/main/java/com/example/t5/components/Header.java
+++ src/main/java/com/example/t5/components/Header.java
@@ -1,14 +1,33 @@
 package com.example.t5.components;
 
+import org.apache.tapestry5.alerts.AlertManager;
+import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.PageRenderLinkSource;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
+import org.tynamo.security.services.SecurityService;
+
+import com.example.t5.pages.Index2;
 
 public class Header {
 
 	// -----------> services
 
 	@Inject
+	private SecurityService securityService;
+
+	@Inject
+	private AlertManager alertManager;
+	
+	@Inject
+	private PageRenderLinkSource prls;
+
+	@Inject
 	private JavaScriptSupport javaScriptSupport;
+	
+	@Inject
+	private Messages messages;
 
 	// -----------> components
 
@@ -20,4 +39,17 @@ public class Header {
 		javaScriptSupport.require("bootstrap/collapse");
 		javaScriptSupport.require("bootstrap/dropdown");
 	}
+
+	@OnEvent(value = "logout")
+    Object onLogout()
+    {
+    	securityService.getSubject().logout();
+    	
+    	if (securityService.getSubject().isAuthenticated()) {
+    		alertManager.warn(messages.get("com.example.header.logoutfailed"));
+    	} else {
+    		alertManager.success(messages.get("com.example.header.logoutsuccess"));
+    	}
+    	return prls.createPageRenderLink(Index2.class);
+    }
 }
