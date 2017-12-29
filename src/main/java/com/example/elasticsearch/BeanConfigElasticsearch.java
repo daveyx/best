e5e616b9e788382c38a7a87cef91fe9f7f93da27
daveyx <d.rehle@gmail.com>
2017-12-29 03:16:29 +0100

    componentscan seems not to be mandatory

--- src/main/java/com/example/elasticsearch/BeanConfigElasticsearch.java
+++ src/main/java/com/example/elasticsearch/BeanConfigElasticsearch.java
@@ -1,11 +1,9 @@
 package com.example.elasticsearch;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.example.service")
 public class BeanConfigElasticsearch {
 
 	@Bean
